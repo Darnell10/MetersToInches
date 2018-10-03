6e6b8254ba27299e2c2c3c1f964d36ad0f318aa2
Darnell O <DarnellOtto@ac.c4q.nyc>
2018-10-02 20:17:54 -0400

    Added more logic text collor changes if no value is entered ito the edit text. input is oly numbers etc.

--- app/src/main/java/com/example/d/meterstoinches/MainActivity.java
+++ app/src/main/java/com/example/d/meterstoinches/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.d.meterstoinches;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +32,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //conversion logic
+
+                double muliply = 39.37;
+                double result = 0.0;
+
+                if (metersEntery.getText().toString().equals("")) {
+                    //sets error message if no value is entered
+                    resultText.setText(R.string.error_message);
+                    //sets color of error message
+                    resultText.setTextColor(Color.RED);
+                } else {
+                    //converts numbers entered to doubles
+                    double meterValue = Double.parseDouble(metersEntery.getText().toString());
+                    result = meterValue * muliply;
+
+                    //converts to string
+                    //resultText.setText(Double.toString(result) + " inches ");
+                    resultText.setTextColor(Color.DKGRAY);
+                    resultText.setText(String.format("%.2f", result));
+
+                }
+            }
+        });
     }
 
 
